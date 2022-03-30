4858fa630f9ff85222d76ec235f144d266aeb1d8
enszrlu <enes.zrl@gmail.com>
2022-03-31 00:29:42 +0300

    implementing unit tests

--- components/translator.js
+++ components/translator.js
@@ -3,7 +3,68 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+function swap(json) {
+    var ret = {};
+    for (var key in json) {
+        ret[json[key]] = key;
+    }
+    return ret;
+}
+
+const britishToAmericanSpelling = swap(americanToBritishSpelling);
+const britishToAmericanTitles = swap(americanToBritishTitles);
+
+const americanToBritDict = { ...americanToBritishTitles, ...americanOnly, ...americanToBritishSpelling }
+
+const britishToAmericanDict = { ...britishToAmericanTitles, ...britishOnly, ...britishToAmericanSpelling }
+
 class Translator {
+    translate(text, dict) {
+        let searchText = text.toLowerCase();
+        let translatedText = text;
+
+        Object.keys(dict).forEach((key) => {
+            if (searchText.includes(key)) {
+                let re = new RegExp(key, "ig");
+                translatedText = translatedText.replace(re, '<span class="highlight">' + dict[key] + '</span>');
+                searchText = translatedText.toLowerCase();
+            }
+        })
+
+        if (translatedText.charAt(0) == "<") {
+            translatedText = translatedText.slice(0, 24) + translatedText.charAt(24).toUpperCase() + translatedText.slice(25);
+        }
+        else {
+            translatedText = translatedText.charAt(0).toUpperCase() + translatedText.slice(1);
+        }
+
+        console.log("Text: " + text)
+        console.log("Translated Text: " + translatedText)
+        return translatedText;
+    }
+
+    americanToBritishTranslator(text) {
+        //let regex = /[ .:;?!~,`"&|()<>{}\[\]\r\n/\\]+/
+        //let words = text.split(regex);
+
+        let translatedText = this.translate(text, americanToBritDict);
+
+        let timeRegex = /(\d\d):(\d\d)/;
+        translatedText = translatedText.replace(timeRegex, '<span class="highlight">' + "$1" + "." + "$2" + '</span>');
+
+        return translatedText
+    }
+
+    britishToAmericanTranslator(text) {
+
+        let translatedText = this.translate(text, britishToAmericanDict);
+
+        let timeRegex = /(\d\d).(\d\d)/;
+        translatedText = translatedText.replace(timeRegex, '<span class="highlight">' + "$1" + ":" + "$2" + '</span>');
+
+        return translatedText
+    }
+
 
 }
 
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,40 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let text = req.body.text;
+      let locale = req.body.locale;
+
+
+      if (text == "") {
+        return res.json({ error: 'No text to translate' })
+      }
+      if (!text || !locale || locale == "") {
+        return res.json({ error: 'Required field(s) missing' })
+      }
+
+
+      if (locale == "american-to-british") {
+        let translation = translator.americanToBritishTranslator(text);
+        if (translation.toLowerCase() == text.toLowerCase()) {
+          return res.json({ text: text, translation: "Everything looks good to me!" })
+        }
+        res.json({ text: text, translation: translation })
+      }
+      else if (locale == "british-to-american") {
+        let translation = translator.britishToAmericanTranslator(text);
+        if (translation.toLowerCase() == text.toLowerCase()) {
+          return res.json({ text: text, translation: "Everything looks good to me!" })
+        }
+        res.json({ text: text, translation: translation })
+      }
+      else {
+        return res.json({ error: 'Invalid value for locale field' })
+      }
+
     });
 };
